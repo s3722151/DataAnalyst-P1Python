51366780a86133fa662eaf2f3219d1c7c6f0580f
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-03-04 20:27:50 +1100

    Created the HelloWorld file and started experimenting with Python

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#Use this: https://www.w3schools.com/python/python_intro.asp
+#How to run the file: https://www.youtube.com/watch?v=OCZe7w2uqJo
+
+#Printing Hello World
+print('Hello World')
+
+#Here is where I will do a calculation 
+print('Calculation Area')
